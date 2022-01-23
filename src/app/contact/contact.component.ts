2df37882e0598712f223cad853be4394f15b9c91
Niklas Zeiler <zeiler.niklas@outlook.de>
2022-01-23 01:31:30 +0100

    email testing

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormControl, FormGroup, Validators, FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,70 +9,72 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ContactComponent {
   emailstring = "mailto:zeiler.niklas@outlook.de";
-  // name = (<HTMLInputElement>document.getElementById('name')).value;
 
-  contact: any = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  // contact = {
+  //   name: '',
+  //   email: '',
+  //   message: '',
+  // };
 
-  post = {
-    endPoint: 'https://www.niklas-zeiler/sendMail.php',
+  // post = {
+  //   endPoint: 'https://www.niklas-zeiler.de/sendMail.php',
 
-    body: (payload: any) => JSON.stringify(payload),
+  //   body: (payload: any) => JSON.stringify(payload),
 
-    options: {
-      headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
-      },
-    },
-  };
+  //   options: {
+  //     headers: {
+  //       'Content-Type': 'text/plain',
+  //       responseType: 'text',
+  //     },
+  //   },
+  // };
 
-  // form: FormGroup;
-  // name: FormControl = new FormControl("", [Validators.required]);
-  // email: FormControl = new FormControl("", [Validators.required, Validators.email]);
-  // message: FormControl = new FormControl("", [Validators.required]);
-  // // honeypot: FormControl = new FormControl(""); // we will use this to prevent spam
-  // submitted: boolean = false;
-  // isLoading: boolean = false;
-  // responseMessage: string;
+  form: FormGroup;
+  name: FormControl = new FormControl("", [Validators.required]);
+  email: FormControl = new FormControl("", [Validators.required, Validators.email]);
+  message: FormControl = new FormControl("", [Validators.required]);
+  // honeypot: FormControl = new FormControl(""); // we will use this to prevent spam
+  submitted: boolean = false;
+  isLoading: boolean = false;
+  responseMessage: string;
 
-  constructor(private http: HttpClient) { }
+  // constructor(private http: HttpClient) { }
+  // ngOnInit(): void { }
 
-    onSubmit(ngForm) {
-    if (ngForm.submittes && ngForm.form.valid) {
-      this.http
-        .post(this.post.endPoint, this.post.body(this.contact))
-        .subscribe({
-          next: (response) => console.log(response),
-          error: (error) => console.error(error),
-          complete: () => console.info('sendpost complete'),
-        });
-    }
-  }
 
-  // constructor(private formBuilder: FormBuilder, private http: HttpClient) {
-  //   this.form = this.formBuilder.group({
-  //     name: this.name,
-  //     email: this.email,
-  //     message: this.message,
-  //   });
-  // }
-  // OnSubmit() {
-
-  //   if (this.form.status == "VALID") {
-  //     this.form.disable();
-  //     let formData: any = new FormData();
-  //     formData.append("name", this.form.get("name").value);
-  //     formData.append("email", this.form.get("email").value);
-  //     formData.append("message", this.form.get("message").value);
-  //     this.http.post("https://niklas-zeiler.de/send_mail.php", formData).subscribe();
-  //     alert('Thanks for the message! I`ll get back to you soon!');
-
+  // onSubmit(ngForm: any) {
+  //   if (ngForm.submitted
+  //    && ngForm.form.valid) {
+  //     this.http
+  //       .post(this.post.endPoint, this.post.body(this.contact))
+  //       .subscribe({
+  //         next: (response) => console.log(response),
+  //         error: (error) => console.error(error),
+  //         complete: () => console.info('sendpost complete'),
+  //       });
   //   }
-  //   this.form.reset();
   // }
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.form = this.formBuilder.group({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+  }
+  OnSubmit() {
+
+    if (this.form.status == "VALID") {
+      this.form.disable();
+      let formData: any = new FormData();
+      formData.append("name", this.form.get("name").value);
+      formData.append("email", this.form.get("email").value);
+      formData.append("message", this.form.get("message").value);
+      this.http.post("https://niklas-zeiler.de/send_mail.php", formData, {responseType: 'text'}).subscribe();
+      alert('Thanks for the message! I`ll get back to you soon!');
+
+    }
+    this.form.reset();
+  }
 
 }
